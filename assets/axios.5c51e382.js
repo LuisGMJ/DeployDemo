e824ea5b4bffba3f6ad9e2441aa72ae81fbfb05a
luisQuemari <luis.quemari@gmail.com>
2024-01-02 01:50:58 -0600

    First commit

---
+++ assets/axios.5c51e382.js
@@ -0,0 +1,1 @@
+import{b as i}from"./index.f90bf49e.js";import{a}from"./axios.b571cc05.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
